371a1e292052d354c99f91a1cf643c53003a8cdf
Tucker Connelly <web@tuckerconnelly.com>
2017-04-02 22:07:47 -0700

    Add elevations

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export {default as Ripples} from './components/Ripples'
 
 // styles
 export {default as animations} from './styles/animations'
+export {default as elevations} from './styles/elevations'
 export {default as breakpoints} from './styles/breakpoints'
 export {default as colors} from './styles/colors'
 
--- src/themes/_base.js
+++ src/themes/_base.js
@@ -1,5 +1,6 @@
 import colors from '../styles/colors'
 import breakpoints from '../styles/breakpoints'
+import elevations from '../styles/elevations'
 
 export default `
   html, body, #__next, [data-reactroot] {
@@ -70,49 +71,13 @@ export default `
   }
 
 
-  .dp0 {
-    box-shadow:
-      0 0px 0px 0px rgba(0, 0, 0, 0),
-      0 0px 0px 0px rgba(0, 0, 0, 0);
-  }
-  .dp2 {
-    box-shadow:
-      0 2px 2px 0px rgba(0, 0, 0, 0.14),
-      0 3px 1px -2px rgba(0, 0, 0, 0.2);
-  }
-  .dp3 {
-    box-shadow:
-      0 3px 4px 0px rgba(0, 0, 0, 0.14),
-      0 3px 3px -2px rgba(0, 0, 0, 0.2);
-  }
-  .dp4 {
-    box-shadow:
-      0 4px 5px 0px rgba(0, 0, 0, 0.14),
-      0 1px 10px -2px rgba(25, 184, 222, 0.2);
-  }
-  .dp6 {
-    box-shadow:
-      0 6px 10px 0px rgba(0, 0, 0, 0.14),
-      0 1px 18px 0px rgba(0, 0, 0, 0.2);
-  }
-  .dp8 {
-    box-shadow:
-      0 6px 10px 0 rgba(0, 0, 0, 0.14),
-      0 3px 5px -1px rgba(0, 0, 0, 0.2);
-  }
-  .dp12 {
-    box-shadow:
-      0 12px 16px 1px rgba(0, 0, 0, 0.14),
-      0 3px 22px 3px rgba(0, 0, 0, 0.2);
-  }
-  .dp16 {
-    box-shadow:
-      0 16px 24px 2px rgba(0, 0, 0, 0.14),
-      0 6px 30px 5px rgba(0, 0, 0, 0.2);
-  }
-  .dp24 {
-    box-shadow:
-      0 24px 32px 3px rgba(0, 0, 0, 0.14),
-      0 12px 36px 8px rgba(0, 0, 0, 0.2);
-  }
+  .dp0 { ${elevations.dp0} }
+  .dp2 { ${elevations.dp2} }
+  .dp3 { ${elevations.dp3} }
+  .dp4 { ${elevations.dp4} }
+  .dp6 { ${elevations.dp6} }
+  .dp8 { ${elevations.dp8} }
+  .dp12 { ${elevations.dp12} }
+  .dp16 { ${elevations.dp16} }
+  .dp24 { ${elevations.dp24} }
 `
