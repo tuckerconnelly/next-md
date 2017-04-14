e4b8a1aa10c34a5e8c0f2c38958d5507cd976e5f
Jacob Price <J@cobprice.com>
2017-04-13 22:03:44 -0400

    Color prop added to styling. Single line style . Props spaced for clearer code.

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -2,15 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Divider = ({color, style, ...other}) => (
-  <hr
-    style={{
-      borderTopColor: 'rgba(0, 0, 0, .12)',
-      ...style,
-    }}
-  />
+  <hr style={{borderTopColor: color || 'rgba(0, 0, 0, 0.12)', ...style}} />
 )
+
 Divider.propTypes = {
   color: PropTypes.string,
+
   style: PropTypes.object,
 }
 
