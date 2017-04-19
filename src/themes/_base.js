d27624e543d8fb5ff6244de6a238e6fa51692044
Tucker Connelly <web@tuckerconnelly.com>
2017-04-19 06:08:51 -0700

    Idk I did this like a week ago

--- src/themes/_base.js
+++ src/themes/_base.js
@@ -6,12 +6,16 @@ import elevations from '../styles/elevations'
 
 export default theme =>
   `
-  html, body, #__next, [data-reactroot] {
+  html, body, #__next {
     height: 100%;
     margin: 0;
 
     color: ${colors.textBlack};
     font-family: Roboto, sans-serif;
+  }
+
+  [data-reactroot] {
+    min-height: 100%;
   }
 
   * {
