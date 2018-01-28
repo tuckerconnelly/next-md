cb521f8952dade2b0a0d9ea443a07758be35adf5
Tucker Connelly <web@tuckerconnelly.com>
2018-01-27 16:20:12 -0800

    Remove transitionify

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
-import transitionify from 'transitionify'
+import Transition from 'react-transition-group/Transition'
 
 class Dialog extends React.Component {
   state = {mounted: false}
@@ -23,83 +23,87 @@ class Dialog extends React.Component {
     const {children, active, onClose} = this.props
 
     return ReactDOM.createPortal(
-      <div className={`dialog ${active ? 'active' : ''}`}>
-        <div
-          className='background'
-          onClick={onClose} />
-        <div className='body'>
-          {children}
-        </div>
-        <style jsx>{`
-          .dialog {
-            position: absolute;
-            left: 0;
-            top: 0;
+      <Transition timeout={300} in={active}>
+        {state => (
+          <div className={`dialog ${state}`}>
+            <div
+              className='background'
+              onClick={onClose} />
+            <div className='body'>
+              {children}
+            </div>
+            <style jsx>{`
+              .dialog {
+                position: absolute;
+                left: 0;
+                top: 0;
 
-            width: 100%;
-            height: 100%;
+                width: 100%;
+                height: 100%;
 
-            display: flex;
-            flex-direction: column;
-            align-items: stretch;
-          }
+                display: flex;
+                flex-direction: column;
+                align-items: stretch;
+              }
 
-          .background {
-            position: absolute;
-            left: 0;
-            top: 0;
+              .background {
+                position: absolute;
+                left: 0;
+                top: 0;
 
-            width: 100%;
-            height: 100%;
+                width: 100%;
+                height: 100%;
 
-            background-color: rgba(0, 0, 0, 0);
+                background-color: rgba(0, 0, 0, 0);
 
-            cursor: pointer;
+                cursor: pointer;
 
-            ${this.context.nextMdTheme.animations.standard('background-color')}
-          }
+                ${this.context.nextMdTheme.animations.standard('background-color')}
+              }
 
-          .active .background {
-            background-color: rgba(0, 0, 0, 0.28);
-          }
+              .entering .background, .entered .background {
+                background-color: rgba(0, 0, 0, 0.28);
+              }
 
-          .body {
-            padding: ${this.context.nextMdTheme.g(2)} ${this.context.nextMdTheme.g(4)};
-            border-radius: 2px;
-            margin-left: ${this.context.nextMdTheme.g(2)};
-            margin-right: ${this.context.nextMdTheme.g(2)};
-            margin-top: ${this.context.nextMdTheme.g(0)};
+              .body {
+                padding: ${this.context.nextMdTheme.g(2)} ${this.context.nextMdTheme.g(4)};
+                border-radius: 2px;
+                margin-left: ${this.context.nextMdTheme.g(2)};
+                margin-right: ${this.context.nextMdTheme.g(2)};
+                margin-top: ${this.context.nextMdTheme.g(0)};
 
-            background-color: white;
+                background-color: white;
 
-            opacity: 0;
-            ${this.context.nextMdTheme.elevations.dp8}
+                opacity: 0;
+                ${this.context.nextMdTheme.elevations.dp8}
 
-            ${this.context.nextMdTheme.animations.standard('margin-top opacity')}
-          }
+                ${this.context.nextMdTheme.animations.standard('margin-top opacity')}
+              }
 
-          .active .body {
-            margin-top: ${this.context.nextMdTheme.g(2)};
+              .entering .body, .entered .body {
+                margin-top: ${this.context.nextMdTheme.g(2)};
 
-            opacity: 1;
-          }
+                opacity: 1;
+              }
 
-          @media ${this.context.nextMdTheme.breakpoints.sm} {
-            .dialog {
-              align-items: center;
-            }
+              @media ${this.context.nextMdTheme.breakpoints.sm} {
+                .dialog {
+                  align-items: center;
+                }
 
-            .body {
-              width: ${this.context.nextMdTheme.g(130)};
-              margin-top: ${this.context.nextMdTheme.g(8)};
-            }
+                .body {
+                  width: ${this.context.nextMdTheme.g(130)};
+                  margin-top: ${this.context.nextMdTheme.g(8)};
+                }
 
-            .active .body {
-              margin-top: ${this.context.nextMdTheme.g(10)};
-            }
-          }
-        `}</style>
-      </div>,
+                .entering .body, .entered .body {
+                  margin-top: ${this.context.nextMdTheme.g(10)};
+                }
+              }
+            `}</style>
+          </div>
+        )}
+      </Transition>,
       this.el
     )
   }
@@ -107,10 +111,8 @@ class Dialog extends React.Component {
 
 Dialog.propTypes = {
   children: PropTypes.node,
-  onClose: PropTypes.func,
-
-  // transition
-  active: PropTypes.bool
+  active: PropTypes.bool,
+  onClose: PropTypes.func
 }
 
 Dialog.contextTypes = {
@@ -121,4 +123,4 @@ Dialog.defaultProps = {
   pathname: '/'
 }
 
-export default transitionify()(Dialog)
+export default Dialog
--- src/components/FormError.js
+++ src/components/FormError.js
@@ -1,51 +1,66 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import transitionify from 'transitionify'
+import Transition from 'react-transition-group/Transition'
 
 import MD_COLORS from '../MD_COLORS'
 
+const TRANSITION_TIMEOUT = 300
+
 class FormError extends React.Component {
+  state = {children: this.props.children}
+
+  componentWillReceiveProps (nextProps) {
+    if (this.props.children && !nextProps.children) {
+      setTimeout(() => this.setState({children: nextProps.children}), TRANSITION_TIMEOUT)
+    }
+
+    if (!this.props.children && nextProps.children) {
+      this.setState({children: nextProps.children})
+    }
+  }
+
   render () {
-    const {active, children} = this.props
+    const {children} = this.state
     return (
-      <div className={active && 'active'}>
-        {children}
-        <style jsx>{`
-          div {
-            max-height: 0;
-            margin-bottom: ${this.context.nextMdTheme.g(0)};
+      <Transition timeout={TRANSITION_TIMEOUT} in={!!this.props.children}>
+        {state => (
+          <div className={state}>
+            {children}
+            <style jsx>{`
+              div {
+                max-height: 0;
+                margin-bottom: ${this.context.nextMdTheme.g(0)};
 
-            ${this.context.nextMdTheme.type.body1}
-            color: ${MD_COLORS.red600};
-            font-weight: 300;
+                ${this.context.nextMdTheme.type.body1}
+                color: ${MD_COLORS.red600};
+                font-weight: 300;
 
-            opacity: 0;
+                opacity: 0;
 
-            ${this.context.nextMdTheme.animations.standard('opacity max-height margin-bottom')}
-            transition-delay: 0ms, 50ms, 50ms;
-          }
+                ${this.context.nextMdTheme.animations.standard('opacity max-height margin-bottom')}
+                transition-delay: 0ms, 50ms, 50ms;
+              }
 
-          .active {
-            max-height: ${this.context.nextMdTheme.g(8)};
-            margin-bottom: ${this.context.nextMdTheme.g(2)};
+              .entering, .entered {
+                max-height: ${this.context.nextMdTheme.g(8)};
+                margin-bottom: ${this.context.nextMdTheme.g(2)};
 
-            opacity: 1;
-          }
-        `}</style>
-      </div>
+                opacity: 1;
+              }
+            `}</style>
+          </div>
+        )}
+      </Transition>
     )
   }
 }
 
 FormError.propTypes = {
-  children: PropTypes.node,
-
-  // transitionify
-  active: PropTypes.bool
+  children: PropTypes.node
 }
 
 FormError.contextTypes = {
   nextMdTheme: PropTypes.object
 }
 
-export default transitionify({useChildren: true})(FormError)
+export default FormError
